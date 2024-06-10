4de2cdc03f04c531dcea62a6acffbb0ae19ed932
asqarovilhomjon <asqarovilhomjon045@gmail.com>
2024-06-10 09:50:02 +0500

    masala

--- js/main.js
+++ js/main.js
@@ -4,65 +4,76 @@
 // }
 //   console.log(perimetr(5));
   
+// 2-masala
 // function yuza(a) {
 //     return a * a;
 // }
 //   console.log(yuza(7));
   
+// 3-masala
 // function perimetr(a, b) {
 //     let s = a * b;
 //     let p = 2 * (a + b);
-//     return `yuzasi: ${s}  perimetri ${p}   `;
+//     return ` yuzasi: ${s}  perimetri ${p} `;
 // }
 //   console.log(perimetr(7, 8));
   
-function aylana(d) {
-    let l = 3.14 * d;
-    return `aylananing uzunligi: ${d}     `;
-}
-  console.log(aylana(6));
+// 4-masala
+// function diametr(d) {
+//     п = 3.14
+//     let L = п * d;
+//     return ` aylananing uzunligi: ${d} `;
+// }
+//   console.log(diametr(6));
   
-function kub(a) {
-    let V = a * a * a;
-    let S = 6 * a * a;
-    return `hajmi: ${V}  to'la sirt ${S}   `;
-}
-  console.log(kub(3));
+// 5-masala
+// function yon(a) {
+//     let V = a * a * a;
+//     let S = 6 * a * a;
+//     return `hajmi: ${V}  to'la sirt ${S}   `;
+// }
+//   console.log(yon(3));
   
-function paralapepet(a, b, c) {
-    let V = a * b * c;
-    let S = 2 * (a * b + b * c + a * c);
-    return `hajmi: ${V}  to'la sirt ${S}   `;
-}
-  console.log(paralapepet(5, 7, 6));
+// 6-masala
+// function paralelepeped(a, b, c) {
+//     let V = a * b * c;
+//     let S = 2 * (a * b + b * c + a * c);
+//     return `hajmi: ${V}  to'la sirt ${S}   `;
+// }
+//   console.log(paralelepeped(3, 5, 7));
   
-function doira(a) {
-    let L = 2 * 3.14 * a;
-    let S = 3.14 * a * a;
-    return `uzunligi: ${L}  yuzasi ${S}   `;
-}
-  console.log(doira(5));
+// 7-masala
+// function radius(a) {
+//     п =3.14
+//     let L = 2 * п * a;
+//     let S = п * a * a;
+//     return ` uzunligi: ${L}  yuzasi ${S} `;
+// }
+//   console.log(radius(9));
 
-  function number(a, b) {
-    let d = (a + b) / 2;
+// 8-masala
+// function son(a, b) {
+//     let d = (a + b) / 2;
   
-    return `orta arfmetik ${d}   `;
-}
-  console.log(number(5, 6));
+//     return `orta arfmetik ${d}   `;
+// }
+//   console.log(son(8, 9));
   
-function ildiz(a, b) {
-    let n = Math.sqrt(a * b);
-    return `ildiz ${n}`
+// 9-masala
+// function ildiz(a, b) {
+//     let n = Math.sqrt(a * b);
+//     return `ildiz ${n}`
   
-}
-  console.log(ildiz(10, 7));
-  
-function nol(a, b) {
-    let d = a + b;
-    let c = a * b;
-    let v = a * a;
-    let h = b * b;
-    return `yigindisi ${d} ko'paytmasi ${c}  kavadrati 1-${v} 2-${h} `;
-}
-  console.log(nol(8, 7));
+// }
+//   console.log(ildiz(13, 7));
+ 
+// 10-masala
+// function nol(a, b) {
+//     let d = a + b;
+//     let c = a * b;
+//     let v = a * a;
+//     let h = b * b;
+//     return `yigindisi ${d} ko'paytmasi ${c}  kavadrati ${v} ${h} `;
+// }
+//   console.log(nol(4, 6));
   
